2b9379c5fbea02c8b82d3be126aa59416ec8cf96
syagawa <yagawa@qzc.co.jp>
2019-12-24 16:46:30 +0900

    Add browser_name

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const open = require("open");
 const links = require("./links.js");
+const browser_name = "firefox";
 
 console.log(links);
 
 async function run(){
   for(let i = 0; i < links.length; i++){
-    await open(`https://www.google.co.jp/search?q=${links[i]}&source=lnms&tbm=isch`, {app: 'firefox'});
+    await open(`https://www.google.co.jp/search?q=${links[i]}&source=lnms&tbm=isch`, {app: browser_name});
   }
 }
 
